5f34ca51c3662463ae50aefc2819ec654d27aa66
Denisse Lara <dmlaramartin@gmail.com>
2015-02-14 20:28:07 -0600

    Added empty values for Map.places

--- adventure-fair/fair.py
+++ adventure-fair/fair.py
@@ -6,14 +6,28 @@ STARTING_MONEY = 50
 
 Map.places = {
 	'entrance': Place(
-		greeting="You arrive at the entrance. "
+		name = "Seven Wonders Local Fair",
+		greeting="You are at the entrace of the fair.",
+		farewell="You leave the fair and head towards your home."
 		),
-	'riddles': Riddles(),
-	'guess the number': GuessNumber(),
-	'monster exhibition': Exhibition()
+	'riddles': Riddles(
+		name = "Mr. D's riddles",
+		greeting = "",
+		farewell = ""
+		),
+	'guess the number': GuessNumber(
+		name = "",
+		greeting = "",
+		farewell = ""
+		),
+	'monster exhibition': Exhibition(
+		name = "",
+		greeting = "",
+		farewell = ""
+		)
 }
 
-place_map = Map()
+place_map = Map('entrance')
 a_game = Engine(place_map)
 a_game.intro()
 a_game.play()
